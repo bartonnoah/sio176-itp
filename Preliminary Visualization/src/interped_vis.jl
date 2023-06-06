0fa117f1ae61f7cffb3fa856d0357c6384311733
Jesse <jesbrirob@gmail.com>
2023-06-05 19:52:24 -0700

    Interped data visualization

---
+++ Preliminary Visualization/src/interped_vis.jl
@@ -0,0 +1,27 @@
+using MAT, Plots, GibbsSeaWater, Dates, ColorSchemes
+
+pyplot()
+cd(@__DIR__)
+
+datafile = "../../data/interpolated_vars.mat"
+visdir = "../vis"
+
+data = matread(datafile)
+
+milliseconds_in_day = Dates.value(Millisecond(Day(1)))
+
+times = @. Millisecond(round(Int, data["time"] * milliseconds_in_day)) + DateTime(0,1,1)
+
+varnames = ["Absolute salinity", "Conservative temperature", "Potential density anomaly"]
+vardata = [data["abs_sal_denoised"], data["cons_temp_denoised"], data["pot_dens_anom"]]
+varunits = ["g/kg", "ºC", "kg/m^3"]
+colors = [:haline, :thermal, :dense]
+
+depth=data["depth"]
+times_expanded = repeat(times, size(depth, 1))
+
+for (name, data, unit, color) in zip(varnames, vardata, varunits, colors)
+    p = contourf(times_expanded, depth, data; title = "Interped $name vs depth and time", xlabel = "time", ylabel = "Depth (m)", colorbar_title = "$name ($unit)", yflip=true, c = color, dpi = 1200)
+    display(p)
+    savefig(p,joinpath(visdir, "interp_$name.png"))
+end
